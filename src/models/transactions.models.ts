a0a35c79443ce946b1d78d52861761ddad522396
Fernanda de Souza <a.fernandadesouza@gmail.com>
2023-02-14 20:40:29 -0300

    first commit

---
+++ src/models/transactions.models.ts
@@ -0,0 +1,55 @@
+import { v4 as createUuid } from "uuid";
+
+export const transactionTypes = ["Income", "Outcome"] as const;
+
+type TypeTransaction = typeof transactionTypes[number];
+
+export class Transaction {
+  private _id: string;
+
+  constructor(
+    private _title: string,
+    private _value: number,
+    private _type: TypeTransaction
+  ) {
+    this._id = createUuid();
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  //   getter e setter _title
+  public get title(): string {
+    return this._title;
+  }
+
+  public set title(title: string) {
+    this._title = title;
+  }
+
+  public get value(): number {
+    return this._value;
+  }
+
+  public set value(value: number) {
+    this._value = value;
+  }
+
+  public get type() {
+    return this._type;
+  }
+
+  public set type(type: TypeTransaction) {
+    this._type = type;
+  }
+
+  public toJson() {
+    return {
+      id: this._id,
+      title: this._title,
+      value: this._value,
+      type: this._type,
+    };
+  }
+}
